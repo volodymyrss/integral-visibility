62f66eb615f9f6b2ea5b186e83fd5ab111afba20
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-23 14:51:48 +0200

    removed duplicate method

--- integralvisibility.py
+++ integralvisibility.py
@@ -1,6 +1,8 @@
 import pyfits
 import healpy
 import healtics
+
+
 from integralclient import converttime
 import ephem
 from astropy.coordinates import SkyCoord
@@ -9,21 +11,18 @@ import ephem
 from numpy import *
 
 class Visibility:
+    def __init__(self,minsolarangle=30):
+        self.ijd02=float(converttime("UTC","2002-01-01T00:00:00","IJD"))
+        self.ijd12=float(converttime("UTC","2012-01-01T00:00:00","IJD"))
+
     def get_grid(self,nsides):
         theta,phi=healpy.pix2ang(nsides,arange(healpy.nside2npix(nsides)))
-        return SkyCoord(phi,theta,1,unit=u.rad,representation="physicsspherical")#.represent_as('spherical')
+        return SkyCoord(phi,theta,1,unit=u.rad,representation="physicsspherical")
         
     def for_time(self,t,coord=None,nsides=16):
         ijd=float(converttime("ANY",t,"IJD"))
-        
-        r101=1318.470164166667
-        r1101=4309.546263703704
-        dr=(r1101-r101)/1000.
-        r0=r101-101*dr
 
-        ijd02=float(converttime("UTC","2002-01-01T00:00:00","IJD"))
-        ijd12=float(converttime("UTC","2012-01-01T00:00:00","IJD"))
-        print ijd02,ijd12
+        ijd02,ijd12=self.ijd02,self.ijd12
 
         yeard=(ijd12-ijd02)/10
 
@@ -31,7 +30,7 @@ class Visibility:
         doy=ijd-ijd02-year*yeard
         
         if coord is None:
-            print "get grid.."
+            print "get grid..."
             coord=self.get_grid(nsides)        
 
         
@@ -45,16 +44,7 @@ class Visibility:
         print "will compute map..",coord.shape
         
         sep=coord.separation(sun_coord).deg
-        vmap[(sep>90-30) & (sep<90+30)]=1
+        vmap[(sep>90-self.minsolarangle) & (sep<90+self.minsolarangle)]=1
         
         return vmap
-        
-        #inyz=SkyCoord(
-        #    sun_coord.represent_as('cartesian').x-scx.represent_as('cartesian').x*proj_sun_scx,
-        #    sun_coord.represent_as('cartesian').y-scx.represent_as('cartesian').y*proj_sun_scx,
-        #    sun_coord.represent_as('cartesian').z-scx.represent_as('cartesian').z*proj_sun_scx,
-        #    representation="cartesian")
-
-        
-        
         
