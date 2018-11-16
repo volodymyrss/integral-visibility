312ec39927fc8766e2ce45166e68e2823f473f77
Volodymyr <vladimir.savchenko@gmail.com>
2018-11-16 08:41:53 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='integral-visibility',
+      author='V.S.',
+      py_modules=['integral-visibility'],
+     )
