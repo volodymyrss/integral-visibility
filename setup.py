ed11704d60f6622a66dcd2e99becacbc7c9d4150
Volodymyr <vladimir.savchenko@gmail.com>
2019-01-10 16:21:24 +0100

    remove

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='Distutils',
-      version='1.0',
+      version='0.1',
       description='integral-visibility',
       author='V.S.',
-      py_modules=['integral-visibility'],
+      py_modules=['integralvisibility'],
      )
