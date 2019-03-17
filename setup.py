4604c92782d4f28026a90d0386f5237cadad3c13
Volodymyr <vladimir.savchenko@gmail.com>
2019-03-17 19:41:06 +0100

    typo in name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='Distutils',
+setup(name='integral-visibility',
       version='0.1',
       description='integral-visibility',
       author='V.S.',
