f00ad7d6c2d078b2584e454b5d30c5bec1340968
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-09-06 15:57:28 +0200

    auth when auth

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
         name='integral-visibility',
-        version='1.0.0',
+        version='1.0.1',
         py_modules= ['integralvisibility'],
         url="http://odahub.io",
         package_data     = {
@@ -18,4 +18,9 @@ setup(
             },
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
         long_description=open('README.md').read(),
+        install_requires=[
+                'ephem',
+                'healtics',
+                'healpy'
+            ]
         )
